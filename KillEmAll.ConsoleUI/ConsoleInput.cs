2713a3c3796cb6a5fa5ea0d64330d0766fb0a63c
rossitche <rossitza.nikolova@gmail.com>
2014-02-26 13:01:13 +0200

    Added user command handling methods to ConsoleInput and GameManager classes.

--- KillEmAll.ConsoleUI/ConsoleInput.cs
+++ KillEmAll.ConsoleUI/ConsoleInput.cs
@@ -1,38 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using KillEmAll.Common;
 
 namespace KillEmAll.ConsoleUI
 {
-    static class ConsoleInput
+    static public class ConsoleInput
     {
-        public static void TestReadInput(object sender, EventArgs e)
+        private static readonly char[] commandSeparators = new char[] { ' ' };
+        private static readonly StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries;
+
+        public static void ReadCommand(object sender, EventArgs args)
         {
-            Console.ReadLine();
+            GameManager gameManager = (sender as GameManager);
+
+            if (gameManager != null)
+            {
+                var command =
+                    Console.ReadLine().Split(commandSeparators, splitOptions);
+
+                switch (command[0])
+                {
+                    case "goto":
+                        HandleChangeLocation(gameManager, command[1]);
+                        break;
+                    case "fight":
+                        HandleAttackEnemy(gameManager, command[1]);
+                        break;
+                    case "pickup":
+                        HandleItemPickUp(gameManager, command[1]);
+                        break;
+                    case "drop":
+                        HandleItemDrop(gameManager, command[1]);
+                        break;
+                    case "use":
+                        HandleItemUse(gameManager, command[1]);
+                        break;
+                    default:
+                        HandleInvalidCommand(command[0]);
+                        break;
+                }
+            }
         }
 
-        public static void ReadCommand(object sender, EventArgs e)
+        private static void HandleInvalidCommand(string command)
         {
-            string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            string command = input[0];
-            string argument = input[1];
-
-            //switch (command)
-            //{
-            //    case enter:
-            //    case go:
-            //        this.ChangeLocation(argument);
-            //        break;
-            //    case fight:
-            //    case attack:
-            //    case pickup:
-            //    case drop:
-            //    case use:
-            //    default:
-            //        break;
-            //}
+            // TODO: Implement custom application exception Invalid command
+            // throw new InvalidCommandException(command);
         }
+
+        private static void HandleItemUse(GameManager gameManager, string itemName)
+        {
+            gameManager.HandleItemUse(itemName);
+        }
+
+        private static void HandleItemDrop(GameManager gameManager, string itemName)
+        {
+            gameManager.HandleItemDrop(itemName);
+        }
+
+        private static void HandleItemPickUp(GameManager gameManager, string itemName)
+        {
+            gameManager.HandleItemPickUp(itemName);
+        }
+
+        private static void HandleAttackEnemy(GameManager gameManager, string enemyName)
+        {
+            gameManager.HandleAttackEnemy(enemyName);
+        }
+
+        private static void HandleChangeLocation(GameManager gameManager, string locationName)
+        {
+            gameManager.HandleChangeLocation(locationName);
+        }
+
+        //public static void TestReadInput(object sender, EventArgs e)
+        //{
+        //    Console.ReadLine();
+        //}
+
+
     }
 }
